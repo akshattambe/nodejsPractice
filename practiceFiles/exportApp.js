effb186460459e5606134476e62b56be99dfbcc4
Tambe <AKUMARTA@volvocars.com>
2020-06-04 14:39:43 +0200

    Updated

---
+++ practiceFiles/exportApp.js
@@ -0,0 +1,9 @@
+const TestClass = require('./exportEx');
+//const test05 = require('./exportEx');
+const classObj = new TestClass();
+
+
+classObj.test('this is test');
+classObj.test01('this is test01');
+classObj.test02('this is test02');
+//newFuncTest03('this is newFuncTest03');
---
+++ practiceFiles/exportEx.js
@@ -0,0 +1,16 @@
+class TestMeClass{
+    test(message){
+        console.log(message);
+    }
+    
+    test01(message){
+        console.log(message);
+    }
+    
+    test02(message){
+        console.log(message);
+    }
+}
+
+
+module.exports = TestMeClass;
