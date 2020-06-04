285286a9a1dfd2635e6b64f2f26157ca85763900
Tambe <AKUMARTA@volvocars.com>
2020-06-04 13:42:10 +0200

    updated

--- httpClass.js
+++ httpClass.js
@@ -12,5 +12,6 @@ const server = http.createServer((req, res) => {
     }
 });
 var port = 3000;
-server.listen(port);
-console.log('Listning in port : ' + port);
+server.listen(port,() => {
+    console.log('Listning in port : ' + port);
+});
